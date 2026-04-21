5da4e7edb2ca8db56ff64a3ee6566994830ed979
Sonu Mandal <imsonumandal05@gmail.com>
2026-04-21 23:50:31 +0530

    add program-5.py to demonstrate input and output of apple counts

---
+++ program-5.py
@@ -0,0 +1,9 @@
+# 5.Amaan and Ayaan were given x and y apples respectively and then they were asked
+# to write a python program that would input x and y and display how many apples each person had.
+# You must write such a program.
+
+
+x = int(input("Enter the number of apples Amaan has: "))
+y = int(input("Enter the number of apples Ayaan has: "))
+print("Amaan has", x, "apples.")
+print("Ayaan has", y, "apples.")
