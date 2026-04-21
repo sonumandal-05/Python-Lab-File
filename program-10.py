d733788b6fa3c3b565cf38f7ceb99eb892acd4b1
Sonu Mandal <imsonumandal05@gmail.com>
2026-04-21 23:51:16 +0530

    add program-10.py to prompt user for input values of a and b

---
+++ program-10.py
@@ -0,0 +1,9 @@
+# 10.	When writing a command line program for an assignment, Mr. Bruce Wayne wants to
+# write a program that prompts the user to input values for variables a and b, takes input, 
+# and then displays the data. Can you help him in writing such a program?
+
+
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+print("Value of a:", a)
+print("Value of b:", b)
