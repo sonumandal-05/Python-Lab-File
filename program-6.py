f931c13e38e7b063ca1522451165f86f49af0ac6
Sonu Mandal <imsonumandal05@gmail.com>
2026-04-21 23:50:41 +0530

    add program-6.py to demonstrate variable assignment and output of mango and orange counts

---
+++ program-6.py
@@ -0,0 +1,9 @@
+# 6.Kabir bought 20 mangoes and 23 oranges from a shop. 
+# Store these values in variables a and b in a program and using print function display their respective values.
+
+
+
+a=20
+b=23
+print("Mangoes:", a)
+print("Oranges:", b)
