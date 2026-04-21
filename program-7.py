d7e4dc5f8f8818e6a8e8227d23c4416bb62e475f
Sonu Mandal <imsonumandal05@gmail.com>
2026-04-21 23:50:51 +0530

    add program-7.py to calculate total cost of a comb and a mirror based on user input

---
+++ program-7.py
@@ -0,0 +1,9 @@
+# 7.	Karan runs a shop, and he wants to calculate price of 1 comb and 1 mirror which cost x and y respectively, 
+# input x and y from the user and display total cost customer must pay.
+
+
+x = int(input("Enter the price of 1 comb: "))
+y = int(input("Enter the price of 1 mirror: "))
+
+total_cost = x + y
+print("Total cost:", total_cost)
