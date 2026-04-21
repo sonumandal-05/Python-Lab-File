43d83c0efbd51b155a39bce86a74d5b9bcf3ef45
Sonu Mandal <imsonumandal05@gmail.com>
2026-04-21 23:17:48 +0530

    first commit

---
+++ question-1.py
@@ -0,0 +1,9 @@
+#1. This is \\ backslash. (Using escape sequence and raw string)
+
+
+#Example of using escape sequence
+print("This is \\ backslash.  ")
+
+
+#Example of using raw string
+print(r"This is \\ backslash.  ")
