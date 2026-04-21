22cf73c5cc58dfe399291e8ccee50b5e4d6cf7af
Sonu Mandal <imsonumandal05@gmail.com>
2026-04-21 23:51:08 +0530

    add program-9.py to display the prices of four cars sold by Kshitij

---
+++ program-9.py
@@ -0,0 +1,22 @@
+# 9.Kshitij runs a car dealership and he wanted to display the cost of 4 cars which he had sold in the past week
+# so he wrote the code given in the editor. But it was showing “x y a b” instead of the values stored in those variables.
+# Your task is to identify the problem with this code and fix it to print the prices of the cars (each in a new line)
+
+# code:
+# x = input() 
+# y = input()
+# a = input()
+# b = input()
+# print(x+y+a+b)
+
+
+
+
+x=int(input("Enter the price of car 1: "))
+y=int(input("Enter the price of car 2: "))
+a=int(input("Enter the price of car 3: "))
+b=int(input("Enter the price of car 4: "))
+print(x)
+print(y)    
+print(a)
+print(b)
