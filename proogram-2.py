47157e1896838227541698ec7fe8a83da2b971d0
Sonu Mandal <imsonumandal05@gmail.com>
2026-04-21 23:49:58 +0530

    add proogram-2.py with examples of escape sequences and raw strings

---
+++ proogram-2.py
@@ -0,0 +1,11 @@
+# 2.	These are /\/\/\/\/\ mountains. (Using escape sequence and raw string)
+
+
+
+#Example of using escape sequence
+
+print("These are /\/\/\/\/\ mountains.  ")
+
+#Example of using raw string
+
+print(r"These are /\/\/\/\/\ mountains.  ")
