522d97f8a26d9715190b2bbd5b086fa887db0a49
Sonu Mandal <imsonumandal05@gmail.com>
2026-04-21 23:50:59 +0530

    add program-8.py to demonstrate basic arithmetic operations using the Python prompt

---
+++ program-8.py
@@ -0,0 +1,18 @@
+# 8.A friend told Dev that he can use the Python prompt to do some basic arithmetic operations without using the input and output functions.
+# Explain how he will perform the operation 50 + 40 -35 in python.
+
+
+result = 50 + 40 - 35
+result
+
+
+
+
+# Output:-
+
+# PS C:\Users\Sonu\OneDrive\Desktop\Devloper\Python Lab-File> python
+# Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+# Type "help", "copyright", "credits" or "license" for more information.
+# Ctrl click to launch VS Code Native REPL
+# >>> 50+40-35
+# 55
