bf93ede15c3b4ef3d9949a09dd877d1801b2ccba
Sonu Mandal <imsonumandal05@gmail.com>
2026-04-22 17:28:48 +0530

    add question-13.py to ask for user name and count occurrences of a specified character

---
+++ question-13.py
@@ -0,0 +1,10 @@
+# 3.	Ask the user to input their name and any character. 
+# Display the length of name excluding spaces and count of character in name.
+
+
+x = input("Please enter your name: ")
+character = input("Please enter a character to count in your name: ")
+name_length = len(x.replace(" ", ""))
+character_count = x.count(character)
+print("The length of your name excluding spaces is:", name_length)
+print(f"The count of character '{character}' in your name is:", character_count)
