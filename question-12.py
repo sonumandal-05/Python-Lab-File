7f9b869503b3be4740f774260d0dcec8d8d997a5
Sonu Mandal <imsonumandal05@gmail.com>
2026-04-22 17:27:54 +0530

    add question-12.py to ask for user name and display it in upper case

---
+++ question-12.py
@@ -0,0 +1,7 @@
+# 2.	Ask the user to input their name and display all character in upper case.
+
+
+
+x = input("Please enter your name: ")
+upper_case_name = x.upper()
+print("Your name in upper case is:", upper_case_name)
