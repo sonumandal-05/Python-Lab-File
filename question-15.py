fa79bcafffb7dbee14dbbf9178fe25f571e13669
Sonu Mandal <imsonumandal05@gmail.com>
2026-04-23 18:05:42 +0530

    add question-15.py to ask for user input of three numbers and display their average

---
+++ question-15.py
@@ -0,0 +1,9 @@
+# 5.	Ask the users to input three numbers and display the average of numbers.
+
+
+
+num1 = float(input("Please enter the first number: "))
+num2 = float(input("Please enter the second number: ")) 
+num3 = float(input("Please enter the third number: "))
+average = (num1 + num2 + num3) / 3
+print("The average of the three numbers is:", average)
