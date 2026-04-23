2c02a99188b7d4c179f3805db1000cc98cd9a909
Sonu Mandal <imsonumandal05@gmail.com>
2026-04-23 18:05:55 +0530

    add question-16.py to convert temperature from Celsius to Fahrenheit

---
+++ question-16.py
@@ -0,0 +1,7 @@
+# 6.	Ask the user to input temperature in Celsius and display the temperature in Fahrenheit.
+
+
+
+celsius = float(input("Please enter the temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"The temperature in Fahrenheit is: {fahrenheit}")
